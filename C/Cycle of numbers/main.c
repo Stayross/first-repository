8a9a8d62fb7204606d1eeaf1d9b8a7467398e812
stayros1978 <stayros.crypto@gmail.com>
2025-07-01 21:42:50 +0300

    Add files via upload

---
+++ C/Cycle of numbers/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+main()
+{
+	int i,m,k,x,j,A[1000],l,q,s;
+	double m_o,sum;
+
+	printf("dwse to m\n");
+	scanf("%d",&m);
+	printf("dwse to k\n");
+	scanf("%d",&k);
+
+	s=k+1;
+	for(i=0;i<1000;i++)
+	A[i]=0;
+	for(i=m;i<s;i++)
+	{
+		j=1;
+		x=i;
+		while(x!=1)
+		{
+			if(x%2 == 0)
+			{
+				x=x/2;
+			}
+			else
+			{
+				x=x*3+1;
+			}
+			j=j+1;
+		}
+		A[i-m]=j;
+	}
+	sum=0;
+	l=A[0];
+	q=1;
+	for(i=1;i<k-m+1;i++)
+	{
+		if(A[i] >= l)
+		{	
+			l=A[i];
+			q=i+m;
+		}
+		sum = A[i-1] + sum;
+	}
+	sum = sum + A[k-m];
+	m_o=sum/(k-m+1);
+	printf("\napotelesmata:\n");
+	printf("To megisto mikos kuklou einai: M(%d)=%d\n",q,l);
+	printf("To meso mikos kuklou einai: %lf",m_o);
+	scanf("%d",&i);
+}
